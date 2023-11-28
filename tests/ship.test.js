67192e36884fd8c13a0398735e764826918709b0
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-11-28 12:31:18 -0500

    Add ship test

---
+++ tests/ship.test.js
@@ -0,0 +1,13 @@
+const Ship = require("../scripts/ship");
+
+const firstShip = Ship(2);
+firstShip.hit();
+firstShip.hit();
+
+test("ship is created with its length", () => {
+  expect(firstShip.length).toBe(2);
+});
+
+test("ship is sunk", () => {
+  expect(firstShip.isSunk()).toBeFalsy();
+});
