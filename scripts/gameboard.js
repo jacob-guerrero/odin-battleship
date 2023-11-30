bd555459141a67970d4bd1403e6dabbc6ab8b756
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-11-30 10:16:43 -0500

    Add gameboard

---
+++ scripts/gameboard.js
@@ -0,0 +1,26 @@
+const Ship = require("./ship");
+const battleship = Ship(4);
+
+const Gameboard = () => {
+  // const boardSize = 10;
+  const ships = [];
+  const missedAttacks = [];
+
+  const gameBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+
+  return { gameBoard};
+};
+
+module.exports = Gameboard;
