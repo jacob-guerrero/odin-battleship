df8179cefdfd9595436082fdd23f6ab9496bbe15
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-12-07 14:57:25 -0500

    Add renderGameboard and updateCells

--- src/scripts/mainLoop.js
+++ src/scripts/mainLoop.js
@@ -25,16 +25,52 @@ const initializeGame = () => {
 
 // Function to render a game board
 const renderGameboard = (elementId, gameboard) => {
+  const boardElement = document.getElementById(elementId);
+  boardElement.innerHTML = "";
+
+  for (let row = 0; row < 10; row++) {
+    for (let col = 0; col < 10; col++) {
+      const cellValue = gameboard.gameBoard[row][col];
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+
+      // Apply styles based on cell value
+      updateCellValue(cell, cellValue);
+
+      // Add dataset attributes for coordinates
+      cell.dataset.x = row;
+      cell.dataset.y = col;
+
+      // Append the cell to the board
+      boardElement.appendChild(cell);
+    }
+  }
 };
 
 const updateCellValue = (cell, cellValue) => {
+  switch (cellValue) {
+    case -1:
+      cell.style.backgroundColor = "blue";
+      break;
+    case 0:
+      cell.style.backgroundColor = "lightblue";
+      break;
+    case 1:
+      cell.style.backgroundColor = "gray";
+      break;
+    case 2:
+      cell.style.backgroundColor = "red";
+      break;
+    default:
+      break;
+  }
 };
 
 const playerLogic = (gameboard, x, y, player1) => {
 };
 const computerLogic = (gameboard, player2) => {
 };
-// Function to handle user input and game loop
+
 const gameLoop = (player1, player2, player1Gameboard, player2Gameboard) => {
 };
 
